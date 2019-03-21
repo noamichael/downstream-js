e173a8111ef7dba2cd1a2c231424bc674422c3be
Michael Kucinski <michaelkucinski@Michaels-MacBook-Pro-2.local>
2019-03-20 22:36:23 -0400

    Map Collectors

--- src/collectors.ts
+++ src/collectors.ts
@@ -1,15 +1,60 @@
+import { Mapper, Reducer } from "./lambdas";
+
 export interface Collector<T, R> {
     (src: Iterable<T>): R
 }
 
+const identity = (i) => i;
+
 export class Collectors {
+
     static toArray<T>(): Collector<T, T[]> {
+        return (src) => [...src];
+    }
+
+    static toList<T>(): Collector<T, T[]> {
+        return Collectors.toArray();
+    }
+
+    static joining(delimimeter = '', prefix = '', suffix = ''): Collector<string, string> {
+        return (src) => Collectors.toArray()(src)
+            .map(s => `${prefix}${s}${suffix}`)
+            .join(delimimeter);
+    }
+
+    static toMap<K, T, R>(keyMapper: Mapper<T, K>, valueMapper?: Mapper<T, R>, mergeFunction?: Reducer<R>): Collector<T, Map<K, R>> {
+        valueMapper = valueMapper || identity;
         return (src) => {
-            const result = [];
+            const map = new Map<K, R>();
             for (let item of src) {
-                result.push(item);
+                const key = keyMapper(item);
+                const mappedValue = valueMapper(item);
+                if (map.has(key)) {
+                    if (!mergeFunction) {
+                        throw new Error(`Cannot map: duplicate entry found for (${key}) and no merge function defined.`);
+                    }
+                    map.set(key, mergeFunction(mappedValue, map.get(key)))
+                } else {
+                    map.set(key, mappedValue);
+                }
+
             }
-            return result;
+            return map;
+        };
+    }
+
+    static groupingBy<K, T>(keyMapper: Mapper<T, K>): Collector<T, Map<K, T[]>> {
+        return (src) => {
+            const map: Map<K, T[]> = new Map();
+            for (let item of src) {
+                const key = keyMapper(item);
+                if (!map.has(key)) {
+                    map.set(key, []);
+                }
+                const group: T[] = map.get(key);
+                group.push(item);
+            }
+            return map;
         };
     }
 }
